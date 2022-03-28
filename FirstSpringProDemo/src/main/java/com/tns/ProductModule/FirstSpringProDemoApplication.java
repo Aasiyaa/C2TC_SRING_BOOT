cc8e6b1acf2980d37cf8ce973313a0e70aaf71f3
Aasiyaa <94465115+Aasiyaa@users.noreply.github.com>
2022-03-28 11:51:02 +0530

    spring_boot demo project

---
+++ FirstSpringProDemo/src/main/java/com/tns/ProductModule/FirstSpringProDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tns.ProductModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProDemoApplication.class, args);
+	}
+
+}
---
+++ FirstSpringProDemo/src/test/java/com/tns/ProductModule/FirstSpringProDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.ProductModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
